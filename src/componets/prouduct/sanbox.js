11f58b66fa03686b881ec8dfa8c2bab1e6870b0b
mehran <taghavimehran99@gmail.com>
2025-04-25 10:54:16 +0330

    add styel titel product and styel price product add if and styel in card

--- src/componets/prouduct/sanbox.js
+++ src/componets/prouduct/sanbox.js
@@ -1,69 +1,90 @@
 let inputJson=[
     {
+        "id":"1",
         "imag1":"/public/imags/1/322.jpg" ,
         "imag2":"/public/imags/1/444.jpg" ,
         "titel":"Floral Kirby",
+        "priceLin":"",
         "price":"$329.10",
-        "detel-titel":"SALE",
+        "deteltitel":"SALE",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"2",
         "imag1":"/public/imags/2/111.jpg" ,
         "imag2":"/public/imags/2/444.jpg" ,
         "titel":"Open knit switer",
+        "priceLin":"",
         "price":"$29.10",
-        "detel-titel":"SALE",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"3",
         "imag1":"/public/imags/3/222.jpg" ,
         "imag2":"/public/imags/3/322.jpg" ,
         "titel":"Official trendy",
+        "priceLin":"",
         "price":"$350.00-355.00",
+        "deteltitel":"SALE",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     
     },
     {
+        "id":"4",
         "imag1":"/public/imags/4/111.jpg" ,
         "imag2":"/public/imags/4/322.jpg" ,
         "titel":"Frock short",
+        "priceLin":"",
         "price":"$249",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"5",
         "imag2":"/public/imags/5/322.jpg" ,
         "imag1":"/public/imags/5/222.jpg" ,
         "titel":"Sleeve dress",
+        "priceLin":"",
         "price":"$59.10",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"6",
         "imag1":"/public/imags/6/222.jpg" ,
         "imag2":"/public/imags/6/322.jpg" ,
         "titel":"Stylish dress",
+        "priceLin":"",
         "price":"$99.00",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"7",
         "imag1":"/public/imags/7/111.jpg" ,
         "imag2":"/public/imags/7/444.jpg" ,
         "titel":"Body suite",
+        "priceLin":"",
         "price":"$329.10",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
     },
     {
+        "id":"8",
         "imag1":"/public/imags/8/222.jpg" ,
         "imag2":"/public/imags/8/322.jpg" ,
         "titel":"Body suite",
-        "priceLin":"$60",
-        "price":"$60",
+        "priceLin":"$60 ",
+        "price":" $50.10",
+        "deteltitel":"",
         "btn-buy-bascket":"nnn",
         "btn-buy-faviert":"mmm",
         
@@ -100,24 +121,29 @@ for (let i = 0; i < inputJson.length; i++) {
 
  divCard.innerHTML=`
      <div class="cardImag">
-     <a href=""><img src="${vv.imag2}" alt="Hover Image"></a>
+     <a href=""><img src="${vv.imag2}" alt=""></a>
       <button class="parent-icon-part-product"><i class="biii bi-cart-fill icon-part-product"></i></button>
       <button class="parent-icon-part-product1"><i class="biii bi-heart-fill icon-part-product"></i></button>
     </div>
   
+
    
     <div class="cardImag1">
      <a href=""><img src="${vv.imag1}" alt="Default Image"></a>
     </div>
   
-    <div>
-      <p>${vv.titel}</p>
-      <p>${vv.price}</p>
+    <div class="cardBady">
+      <p class="p-titel-product">${vv.titel}</p>
+      <p class="p-price-product"><s>${vv.priceLin}</s>${vv.price}</p>
     </div>
+    <p class="${vv.deteltitel ? 'text-off' : "" }">${vv.deteltitel}</p>
+
     `;
 
 
-
+// if (vv.deteltitel==='sale') {
+//     .text-off
+// }
 
 
     cardBady.innerHTML=`
